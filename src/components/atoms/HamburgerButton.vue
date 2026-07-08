aa40e55979d264a3ecf91d1d2016dcf8a1a9e51a
pinwheelrun <pinwheelrun@gmail.com>
2026-07-08 22:17:34 +0900

    Added SVG license

--- src/components/atoms/HamburgerButton.vue
+++ src/components/atoms/HamburgerButton.vue
@@ -16,6 +16,7 @@ function alertInConstruction() {
     <div class="hamburger-menu">
         <button class="hamburger-btn" type="button" aria-label="Open menu" :aria-expanded="burgerEaten"
             @pointerdown.stop.prevent="$emit('toggle-hamburger')">
+            <!-- License: MIT. Made by Will Kelly: https://www.will-kelly.co.uk/ -->
             <svg width="100%" height="100%" viewBox="0 0 24.96 24.96" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5.2 7.28h14.56 M5.2 12.48h14.56 M5.2 17.68h14.56" stroke="currentColor" stroke-width="2.5"
                     stroke-linecap="round" stroke-linejoin="round" />
